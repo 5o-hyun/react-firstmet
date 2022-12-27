a692b944d284933242d26bd32df4eb97f2d75444
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2022-12-27 22:15:53 +0900

    Feat: styled-components

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import DarkOrLight from "./chapter14/DarkOrLight";
+import Blocks from "./chapter15/Blocks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <DarkOrLight />
+  <Blocks />
   // </React.StrictMode>
 );
 
